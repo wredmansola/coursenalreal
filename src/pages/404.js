7a23993b36a29af14b5f9bf1fb83bff4d9288a17
Wredman Sola <65522339+wredmansola@users.noreply.github.com>
2021-02-01 04:54:22 -0500

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => (
   <Layout>
     <div>
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn&#39;t exist... the sadness. Just go to <a href="https://coursenal.in/">for more</a></p>
     </div>
   </Layout>
 )
