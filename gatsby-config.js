eef1057685b2124f5ee1e6b3bcbdb752ae46e21c
Wredman Sola <65522339+wredmansola@users.noreply.github.com>
2021-02-01 04:18:37 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Best Courses to Download|Coursenal.in',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'get latest all courses that boost your career and help you to grow more by our track',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
