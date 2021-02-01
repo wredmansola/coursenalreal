ff61c5680d7285ad61083f15ab74835c8df62a68
Wredman Sola <65522339+wredmansola@users.noreply.github.com>
2021-02-01 04:03:25 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -96,12 +96,12 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Latest Courses
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
-                      Read more
+                      Get more
                     </Link>
                   </div>
                 </div>
